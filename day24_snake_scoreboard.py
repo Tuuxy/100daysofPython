84f422df2a6cea76daa3d4ed5df9f66c7c45a7be
Tuuxy <hautmont.karys@gmail.com>
2024-10-10 14:55:22 +0200

    day24, updated snake game to save highscore on a data file

---
+++ day24_snake_scoreboard.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        with open("day24_data.txt") as file:
+            self.highscore = int(file.read())
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 250)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("day24_data.txt","w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
